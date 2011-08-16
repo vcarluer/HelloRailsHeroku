9e3089c8ae18815b643494618fc2f72a0b3f4310
Vince <vcarluer@gmail.com>
2011-08-16 16:18:34 +0200

    Hello Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloRailsHeroku::Application.config.secret_token = '744d9f0fc00c3895722939fbd15deaf93e1358cb352d1e5f206d4d1ab90b47825599e91b43b4f9fc30858c58bb12779a739aca0f9d3a613bca791a8e656cc987'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HelloRailsHeroku::Application.config.session_store :cookie_store, :key => '_HelloRailsHeroku_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HelloRailsHeroku::Application.config.session_store :active_record_store
