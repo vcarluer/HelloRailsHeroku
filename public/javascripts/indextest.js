b3cdcdd06426994427310119d697db768c85d16b
Vince <vcarluer@gmail.com>
2011-08-16 17:40:20 +0200

    Sencha touch test

---
+++ public/javascripts/indextest.js
@@ -0,0 +1,33 @@
+Ext.setup({
+     phoneStartupScreen: 'yourImageForTablet.png',
+     icon: 'yourIcon.png',
+     glossOnIcon: true,
+     onReady: function() {
+              var onglets;
+ 
+              onglets = new Ext.TabPanel({
+						tabBar: {
+							dock: 'bottom',
+							layout: {
+								pack: 'center'
+							}
+						},
+                        animation:'slide',
+                        fullscreen:true,
+                        items:[
+                              {
+								title:'Accueil',
+								iconCls:'favorites',
+								cls:'tab1',
+								html:'Accueil',
+							 },
+                              {
+							  title:'Info',
+							  cls:'tab2',
+							  iconCls:'info',
+								html:'Info',
+							 }
+                        ]
+               });
+    }
+});
